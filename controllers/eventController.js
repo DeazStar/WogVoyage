94d0845b0355a73ceea985fe2821af431152656f
DeazStar <deadfromtheborn@gmail.com>
2023-08-15 02:16:33 +0300

    add a function to delete an event and get all events

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -12,6 +12,28 @@ const createEvent = catchAsync(async (req, res, next) => {
   });
 });
 
+const deleteEvent = catchAsync(async (req, res, next) => {
+  await Event.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+const getAllEvents = catchAsync(async (req, res, next) => {
+  const events = await Event.find();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      events,
+    },
+  });
+});
+
 module.exports = {
   createEvent,
+  deleteEvent,
+  getAllEvents,
 };
