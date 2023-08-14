d69f197b6c6ef93d95d25a67d20c8efe991af529
DeazStar <deadfromtheborn@gmail.com>
2023-08-14 19:18:17 +0300

    register the globalErrorHandler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const eventRouter = require('./routes/eventRouter');
+const globalErrorHandler = require('./errors/errorHandler');
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api/v1/events', eventRouter);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
