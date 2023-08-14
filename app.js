dab040a8861801fd3154ea684b99b6a9d9c7c9b4
DeazStar <deadfromtheborn@gmail.com>
2023-08-14 18:32:43 +0300

    register the event route

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const eventRouter = require('./routes/eventRouter');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/v1/events', eventRouter);
 
 module.exports = app;
