737222b96a900fe969b130cb0f16f568bf5afdae
DeazStar <deadfromtheborn@gmail.com>
2023-08-15 17:49:19 +0300

    register the user route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import eventRouter from './routes/eventRouter.js';
+import userRouter from './routes/userRoutes.js';
 import globalErrorHandler from './errors/errorHandler.js';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api/v1/events', eventRouter);
+app.use('/api/v1/users', userRouter);
 
 app.use(globalErrorHandler);
 
