b648f4bd60b2380b128e7ff00c2a945bbd5be570
DeazStar <deadfromtheborn@gmail.com>
2023-08-18 01:50:26 +0300

    add route for forget password and recover password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,9 @@ router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
 router.route('/verifyEmail/:token').patch(authController.verifyEmail);
+
+router.route('/forgetPassword').post(authController.forgetPassword);
+
+router.route('/recoverPassword/:token').patch(authController.recoverPassword);
+
 export default router;
