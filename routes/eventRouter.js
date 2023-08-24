5c1a2bb0f5faacc2dafd1112ea250b05a3f4a762
DeazStar <deadfromtheborn@gmail.com>
2023-08-24 15:55:33 +0300

    add nested route for booking

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -3,13 +3,14 @@
 import express from 'express';
 import eventController from '../controllers/eventController.js';
 import userController from '../controllers/authController.js';
+import bookingRouter from './bookingRoute.js';
 
 const router = express.Router();
 
 router
   .route('/')
   .get(userController.protectRoute, eventController.getAllEvents)
-  .post(eventController.createEvent);
+  .post(userController.protectRoute, eventController.createEvent);
 
 router
   .route('/:id')
@@ -17,4 +18,5 @@ router
   .patch(eventController.updateEvent)
   .delete(eventController.deleteEvent);
 
+router.use('/:eventId/booking', bookingRouter);
 export default router;
