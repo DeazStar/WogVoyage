7f1b2d4873908724efcdd4df308fd53bb4553ef1
DeazStar <deadfromtheborn@gmail.com>
2023-08-24 15:55:49 +0300

    register the booking route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import morgan from 'morgan';
 import eventRouter from './routes/eventRouter.js';
 import userRouter from './routes/userRoutes.js';
+import bookingRouter from './routes/bookingRoute.js';
 import globalErrorHandler from './errors/errorHandler.js';
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/v1/events', eventRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.use(globalErrorHandler);
 
