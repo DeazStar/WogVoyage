3b6042142de6e5c5332003d98e2f473bbd1e91bb
DeazStar <deadfromtheborn@gmail.com>
2023-09-29 16:15:37 +0300

    use multer for even image uploading

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,11 +1,44 @@
 /* eslint-disable import/extensions */
+import multer from 'multer';
 import Features from '../lib/Features.js';
 import Event from '../models/eventModel.js';
+import AppError from '../errors/AppError.js';
 import catchAsync from '../errors/catchAsync.js';
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/events');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    const filename = `event-${req.user._id}-${Date.now()}.${ext}`;
+    req.user.filename = filename;
+    cb(null, filename);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('File must be an image', 400));
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  filter: multerFilter,
+  limits: {
+    fileSize: 5242880,
+  },
+});
+
+const uploadCoverImage = upload.single('imageCover');
 
 const createEvent = catchAsync(async (req, res, next) => {
   const requestBody = { ...req.body };
   requestBody.organizer = req.user;
+  requestBody.imageCover = req.user.filename;
   const event = await Event.create(requestBody);
 
   res.status(201).json({
@@ -43,13 +76,23 @@ const getAllEvents = catchAsync(async (req, res, next) => {
 });
 
 const getEventById = catchAsync(async (req, res, next) => {
-  console.log(req.params);
   const event = await Event.findById(req.params.id);
 
   res.status(200).json({
     status: 'success',
     data: {
       event,
+    },
+  });
+});
+
+const getMyEvents = catchAsync(async (req, res, next) => {
+  const events = await Event.find({ organizer: req.user._id.toString() });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      events,
     },
   });
 });
@@ -74,4 +117,6 @@ export default {
   getAllEvents,
   updateEvent,
   getEventById,
+  getMyEvents,
+  uploadCoverImage,
 };
